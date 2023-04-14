450fc599678616e898cfda603cbfc395592a0d0a
Duncan Baldwin <124105688+DuncanKoji@users.noreply.github.com>
2023-04-13 18:41:34 -0600

    added functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,14 @@
 "use strict";
 
-var Grade3
+var Grades = [
+    "Grade3",
+    "Grade4",
+    "Grade5",
+    "Grade6",
+    "Grade7",
+    "Grade8",
+    "Grade9",
+    "Grade10",
+    "Grade11",
+    "Grade12",
+ ];
