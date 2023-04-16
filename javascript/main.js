3661327fc82e00f0cb26c4136741c4e1f4a3da84
Duncan Baldwin <124105688+DuncanKoji@users.noreply.github.com>
2023-04-16 14:05:04 -0600

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -21,8 +21,24 @@ if (input == Grades[0]) {
     gradethree();
 } else if (input == Grades[1]) {
     gradefour();
-} else {
-}
+} else if (input == Grades[2]) {
+    gradefive();
+} else if (input == Grades[3]) {
+    gradesix();
+} else if (input == Grades[4]) {
+    gradeseven();
+} else if (input == Grades[5]) {
+    gradeeight();
+} else if (input == Grades[6]) {
+    gradenine();
+} else if (input == Grades[7]) {
+    gradeten();
+} else if (input == Grades[8]) {
+    gradeeleven();
+} else if (input == Grades[9]) {
+    gradetweleve();
+else}
+
 
 function gradethree() {
 }
